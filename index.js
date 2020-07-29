ba729c89182431888e2d634b8eb4b2e7050e488f
GoldeneGabel <68955981+GoldeneGabel@users.noreply.github.com>
2020-07-29 15:29:22 +0200

    Update index.js
    Increment version number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pr0ShitSkipper
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       GoldeneGabel
 // @match        https://pr0gramm.com/*
